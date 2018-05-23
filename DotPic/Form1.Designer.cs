debe55fd76f268c730057a57aac3251ded901b82
Sharemon <sharemon@163.com>
2018-05-23 14:44:12 +0800

    Final Version

---
+++ DotPic/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace DotPic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbxOrigin = new System.Windows.Forms.PictureBox();
+            this.pbxDot = new System.Windows.Forms.PictureBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxOrigin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxOrigin
+            // 
+            this.pbxOrigin.Image = ((System.Drawing.Image)(resources.GetObject("pbxOrigin.Image")));
+            this.pbxOrigin.Location = new System.Drawing.Point(49, 45);
+            this.pbxOrigin.Name = "pbxOrigin";
+            this.pbxOrigin.Size = new System.Drawing.Size(512, 512);
+            this.pbxOrigin.TabIndex = 0;
+            this.pbxOrigin.TabStop = false;
+            // 
+            // pbxDot
+            // 
+            this.pbxDot.Image = ((System.Drawing.Image)(resources.GetObject("pbxDot.Image")));
+            this.pbxDot.Location = new System.Drawing.Point(619, 45);
+            this.pbxDot.Name = "pbxDot";
+            this.pbxDot.Size = new System.Drawing.Size(512, 512);
+            this.pbxDot.TabIndex = 0;
+            this.pbxDot.TabStop = false;
+            this.pbxDot.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbxDot_MouseClick);
+            this.pbxDot.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbxDot_MouseMove);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(49, 390);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 12);
+            this.lblStatus.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1214, 611);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pbxDot);
+            this.Controls.Add(this.pbxOrigin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxOrigin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDot)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxOrigin;
+        private System.Windows.Forms.PictureBox pbxDot;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ DotPic/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotPic
+{
+    public partial class Form1 : Form
+    {
+        public class DotDot
+        {
+            public double x;
+            public double y;
+            public double r;
+
+            public DotDot(double x, double y, double r)
+            {
+                this.x = x;
+
+                this.y = y;
+
+                if (r >= 1)
+                    this.r = r;
+                else
+                    this.r = 1;
+            }
+        }
+
+        Bitmap bmpOrg;
+        Bitmap bmpDot;
+        Graphics gc;
+        Brush br;
+        List<DotDot> lDot = new List<DotDot>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Color GetRGBSum(int x, int y, int r)
+        {
+            int sumR = 0;
+            int sumG = 0;
+            int sumB = 0;
+
+            Color cl;
+
+            for (int i = x; i < x + r; i++)
+            {
+                for (int j = y; j < y + r; j++)
+                {
+                    cl = bmpOrg.GetPixel(i, y);
+                    sumR += cl.R;
+                    sumG += cl.G;
+                    sumB += cl.B;
+                }
+            }
+
+            sumR /= (r * r);
+            sumG /= (r * r);
+            sumB /= (r * r);
+
+            cl = Color.FromArgb(sumR, sumG, sumB);
+
+            return cl;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmpOrg = new Bitmap(this.pbxOrigin.Image);
+
+            Color cl = GetRGBSum(0, 0, bmpOrg.Width);
+
+            //this.lblRValue.Text = sumR.ToString();
+            //this.lblGValue.Text = sumG.ToString();
+            //this.lblBValue.Text = sumB.ToString();
+
+            bmpDot = new Bitmap(this.pbxDot.Image);
+            gc = Graphics.FromImage(bmpDot);
+            gc.Clear(Color.White);
+
+            br = new SolidBrush(cl);
+            Rectangle rct = new Rectangle(0, 0, bmpDot.Width, bmpDot.Height);
+
+            gc.FillEllipse(br, rct);
+
+            this.pbxDot.Image = bmpDot;
+
+            lDot.Add(new DotDot(bmpDot.Width / 2, bmpDot.Height / 2, bmpDot.Width / 2));
+        }
+
+        private void pbxDot_MouseClick(object sender, MouseEventArgs e)
+        {
+
+
+        }
+
+        private void pbxDot_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            double mx = 0;
+            double my = 0;
+            double mr = 0;
+            bool flag = false;
+
+            foreach (DotDot mDot in lDot)
+            {
+                if (mDot.r <= 4)
+                    continue;
+
+                if ((Math.Pow((x - mDot.x), 2) + Math.Pow((y - mDot.y), 2)) <= Math.Pow(mDot.r, 2))
+                {
+                    mx = (double)Math.Round(mDot.x);
+                    my = (double)Math.Round(mDot.y);
+                    mr = (double)Math.Round(mDot.r);
+
+                    lDot.Remove(mDot);
+
+                    flag = true;
+
+                    break;
+                }
+            }
+
+            if (flag)
+            {
+                Rectangle rct = new Rectangle((int)(mx - mr), (int)(my - mr), (int)(mr * 2), (int)(mr * 2));
+                br = new SolidBrush(Color.White);
+                gc.FillRectangle(br, rct);
+
+                pbxDot.Image = bmpDot;
+
+
+                Color cl = GetRGBSum((int)(mx - mr), (int)(my - mr), (int)(mr));
+                br = new SolidBrush(cl);
+                rct = new Rectangle((int)(mx - mr), (int)(my - mr), (int)(mr), (int)(mr));
+                gc.FillEllipse(br, rct);
+                lDot.Add(new DotDot(mx - mr / 2, my - mr / 2, mr / 2));
+
+                cl = GetRGBSum((int)(mx - mr), (int)(my), (int)(mr));
+                br = new SolidBrush(cl);
+                rct = new Rectangle((int)(mx - mr), (int)(my), (int)(mr), (int)(mr));
+                gc.FillEllipse(br, rct);
+                lDot.Add(new DotDot(mx - mr / 2, my + mr / 2, mr / 2));
+
+                cl = GetRGBSum((int)(mx), (int)(my - mr), (int)(mr));
+                br = new SolidBrush(cl);
+                rct = new Rectangle((int)(mx), (int)(my - mr), (int)(mr), (int)(mr));
+                gc.FillEllipse(br, rct);
+                lDot.Add(new DotDot(mx + mr / 2, my - mr / 2, mr / 2));
+
+                cl = GetRGBSum((int)(mx), (int)(my), (int)(mr));
+                br = new SolidBrush(cl);
+                rct = new Rectangle((int)(mx), (int)(my), (int)(mr), (int)(mr));
+                gc.FillEllipse(br, rct);
+                lDot.Add(new DotDot(mx + mr / 2, my + mr / 2, mr / 2));
+
+                pbxDot.Image = bmpDot;
+            }
+        }
+    }
+}
